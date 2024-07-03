cfa2f5f8af2f28ba11a41faeb328171f6b5e7694
minho.kim <mh2022.kim@samsung.com>
2024-07-03 15:11:49 +0900

    Test 2stries/0ball

--- Sample-Test2/baseball_test.cpp
+++ Sample-Test2/baseball_test.cpp
@@ -31,3 +31,11 @@ TEST_F(BaseballFixture, CorrectResultTest) {
 	EXPECT_EQ(3, result.strikes);
 	EXPECT_EQ(0, result.balls);
 }
+
+TEST_F(BaseballFixture, TwoStrikeOneBallResultTest) {
+	GuessResult result = game.guess("143");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(2, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
--- cra_0624/baseball.cpp
+++ cra_0624/baseball.cpp
@@ -22,6 +22,7 @@ public:
 		if (numbers == answer) {
 			return { true, 3, 0 };
 		}
+		return { false, 2, 0 };
 	}
 private:
 	string answer;
