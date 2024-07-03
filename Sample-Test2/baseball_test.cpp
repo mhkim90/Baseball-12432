bca0f356a03d1e3c28fa269c2b7d7815132f01f6
minho.kim <mh2022.kim@samsung.com>
2024-07-03 14:27:11 +0900

    Test page 27

--- Sample-Test2/baseball_test.cpp
+++ Sample-Test2/baseball_test.cpp
@@ -1,13 +1,14 @@
 #include "pch.h"
 #include "../cra_0624/baseball.cpp"
 #include <vector>
+#include <string>
 using namespace std;
 
 class BaseballFixture : public ::testing::Test {
 public:
-	Baseball baseball;
+	Baseball game;
 };
 
 TEST_F(BaseballFixture, test) {
-	EXPECT_EQ(1, 1);
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
--- cra_0624/baseball.cpp
+++ cra_0624/baseball.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+
+//class length_error : public exception {
+//public:
+//};
 
 class Baseball {
 public:
-
+	void guess(const string& string) {
+		throw length_error("");
+	}
 private:
 };
