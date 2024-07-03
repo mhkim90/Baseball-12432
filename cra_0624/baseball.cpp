8d8f3a6dad4dfcf7e77b1631ae3556203f906e85
minho.kim <mh2022.kim@samsung.com>
2024-07-03 15:00:32 +0900

    Refactoring page 42

--- cra_0624/baseball.cpp
+++ cra_0624/baseball.cpp
@@ -10,6 +10,16 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& numbers) {
+		checkNumbers(numbers);
+	}
+private:
+	bool isDuplicatedNumber(const string& numbers) {
+		return (numbers[0] == numbers[1] ||
+			numbers[1] == numbers[2] ||
+			numbers[2] == numbers[0]);
+	}
+
+	void checkNumbers(const string& numbers) {
 		if (numbers.size() != 3) {
 			throw length_error("");
 		}
@@ -18,11 +28,8 @@ public:
 			throw invalid_argument("");
 		}
 
-		if (numbers[0] == numbers[1] ||
-			numbers[1] == numbers[2] ||
-			numbers[2] == numbers[0]) {
+		if (isDuplicatedNumber(numbers)) {
 			throw invalid_argument("");
-			}
+		}
 	}
-private:
 };
